3478671cb07e8e85a4eb3ea73eb13ea9931f2f17
Chandan Sharma <63193129+chandsharma@users.noreply.github.com>
2024-12-10 16:27:27 +0530

    Update dag-76-spark.py

--- dag-76-spark.py
+++ dag-76-spark.py
@@ -12,41 +12,51 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-spec = {
-apiVersion: "sparkoperator.k8s.io/v1beta2"
-kind: SparkApplication
-metadata:
-  name: dag-79-pod
-  namespace: default
-spec:
-  type: Scala
-  mode: cluster
-  image: "docker.io/channnuu/chandan_spark:3.5.2"
-  imagePullPolicy: IfNotPresent
-  mainClass: org.apache.spark.examples.SparkPi
-  mainApplicationFile: "local:///opt/spark/examples/jars/spark-examples_2.12-3.5.2.jar"
-  sparkVersion: "3.1.2"
-  restartPolicy:
-    type: Never
-  sparkConf:
-    "spark.eventLog.enabled": "true"
-    "spark.eventLog.dir": "wasb://spark-logs@pocdatabricksdoc.blob.core.windows.net/spark-logs"
-    "spark.kubernetes.authenticate.driver.serviceAccountName": "spark"
-    "spark.hadoop.fs.azure.account.key.pocdatabricksdoc.blob.core.windows.net": "jr63R2MPRqUvSDZ/EtX+sNu32kz2wPVVU9S3wJoXZCx6xNJYI+yesstPOVxoyMuWqX2RmKBZRlZg+AStdYa2HA=="
-  driver:
-    cores: 1
-    memory: "512m"
-    serviceAccount: spark
-    labels:
-      version: "3.1.2"
-    env:
-      - name: SPARK_DRIVER_MEMORY
-        value: "512m"
-  executor:
-    cores: 1
-    instances: 2
-    memory: "512m"
-}
+spec = {{
+  "apiVersion": "sparkoperator.k8s.io/v1beta2",
+  "kind": "SparkApplication",
+  "metadata": {
+    "name": "dag-79-pod",
+    "namespace": "default"
+  },
+  "spec": {
+    "type": "Scala",
+    "mode": "cluster",
+    "image": "docker.io/channnuu/chandan_spark:3.5.2",
+    "imagePullPolicy": "IfNotPresent",
+    "mainClass": "org.apache.spark.examples.SparkPi",
+    "mainApplicationFile": "local:///opt/spark/examples/jars/spark-examples_2.12-3.5.2.jar",
+    "sparkVersion": "3.1.2",
+    "restartPolicy": {
+      "type": "Never"
+    },
+    "sparkConf": {
+      "spark.eventLog.enabled": "true",
+      "spark.eventLog.dir": "wasb://spark-logs@pocdatabricksdoc.blob.core.windows.net/spark-logs",
+      "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
+      "spark.hadoop.fs.azure.account.key.pocdatabricksdoc.blob.core.windows.net": "jr63R2MPRqUvSDZ/EtX+sNu32kz2wPVVU9S3wJoXZCx6xNJYI+yesstPOVxoyMuWqX2RmKBZRlZg+AStdYa2HA=="
+    },
+    "driver": {
+      "cores": 1,
+      "memory": "512m",
+      "serviceAccount": "spark",
+      "labels": {
+        "version": "3.1.2"
+      },
+      "env": [
+        {
+          "name": "SPARK_DRIVER_MEMORY",
+          "value": "512m"
+        }
+      ]
+    },
+    "executor": {
+      "cores": 1,
+      "instances": 2,
+      "memory": "512m"
+    }
+  }
+}}
 
 dag = DAG(
     "dag-76-pod", #same name as application. and there should be no underscore in application name
